8b328668cb1c017cad3f9fb56a85e7ff8dbe0c23
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-06 11:39:19 +0700

    nambain create new menu

---
+++ delivery/controller/menu_controller.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"go_wmb_gin_refactor/delivery/api"
+	"go_wmb_gin_refactor/model"
+	"go_wmb_gin_refactor/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//Nambain usecase
+
+type MenuController struct {
+	router *gin.Engine
+
+	ucCreateMenu usecase.CreateMenuUsecase
+	// ucUpdateMenu usecase.UpdateMenuUsecase
+	// ucDeleteMenu usecase.DeleteMenuUsecase
+
+	api.BaseApi
+}
+
+func (m *MenuController) createNewMenu(c *gin.Context) {
+	var newMenu *model.Menu
+
+	if err := c.BindJSON(&newMenu); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+	} else {
+		err := m.ucCreateMenu.CreateMenu(newMenu)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"status":  "FAILED",
+				"message": "Error when creating Product",
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "success",
+			"message": newMenu,
+		})
+
+	}
+}
+
+func NewMenuController(router *gin.Engine, ucCreateMenu usecase.CreateMenuUsecase) *MenuController {
+	controller := MenuController{
+		router:       router,
+		ucCreateMenu: ucCreateMenu,
+	}
+
+	router.POST("/product", controller.createNewMenu)
+
+	return &controller
+
+}
---
+++ delivery/server.go
@@ -0,0 +1,44 @@
+package delivery
+
+import (
+	"go_wmb_gin_refactor/config"
+	"go_wmb_gin_refactor/manager"
+
+	"github.com/gin-gonic/gin"
+)
+
+type appServer struct {
+	useCaseManager manager.UseCaseManager
+	engine         *gin.Engine
+	host           string
+}
+
+func Server() *appServer {
+	router := gin.Default()
+	appConfig := config.NewConfig()
+
+	infra := manager.NewInfra(appConfig)
+
+	repoManager := manager.NewRepositoryManager(infra)
+	useCaseManager := manager.NewUseCaseManager(repoManager)
+
+	host := appConfig.Url
+
+	return &appServer{
+		useCaseManager: useCaseManager,
+		engine:         router,
+		host:           host,
+	}
+}
+
+func (a *appServer) iniController() {
+
+}
+
+func (a *appServer) Run() {
+	a.iniController()
+	err := a.engine.Run(a.host)
+	if err != nil {
+		panic(err)
+	}
+}
